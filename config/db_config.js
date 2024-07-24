70e4803ad0633b29397ad951eef122997ae3b81b
Josh <viratjosh4@gmail.com>
2024-07-24 12:16:59 +0530

    remove db config details

--- config/db_config.js
+++ config/db_config.js
@@ -1,17 +1,3 @@
-
-// **************** Production  *******************
-
-// module.exports = {
-//     HOST: 'ls-e3363413d7a83d143431db1f60c07b878a96ae1a.crocoq6qec8l.ap-south-1.rds.amazonaws.com',
-//     DATABASE: 'smart_stay_prod',
-//     USER: 'dbmasteruser',
-//     PASSWORD: 'Smartstay!2#0',
-//     multipleStatements: true
-// };
-
-
-// **************** Test  *******************
-
 module.exports = {
     HOST: 'ls-f4c1514e53cc8c27ec23a4ce119af8c49d7b1ce7.crocoq6qec8l.ap-south-1.rds.amazonaws.com',
     DATABASE: 'smart_stay',
