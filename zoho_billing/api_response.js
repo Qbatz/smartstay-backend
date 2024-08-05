60e17f2e73b506bf0debe017e37b7ef059b39013
Josh <viratjosh4@gmail.com>
2024-08-05 16:04:57 +0530

    change status code for the api refresh token response

--- zoho_billing/api_response.js
+++ zoho_billing/api_response.js
@@ -30,7 +30,7 @@ const apiMiddleware = (apiEndpoint, method, input_body) => {
 
                         if (parsedBody.code === 0) {
                             resolve({ shouldRetry: false, response: parsedBody }); // No need to retry, pass the body
-                        } else if (parsedBody.code === 57) {
+                        } else if (parsedBody.code != 57) {
                             resolve({ shouldRetry: false, response: parsedBody }); // No need to retry, pass the body
                         } else {
                             resolve({ shouldRetry: true }); // Indicates that we should retry the API call
