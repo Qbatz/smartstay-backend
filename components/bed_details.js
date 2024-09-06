3e780ca2f4b00edad9a6216ee92bb95c1151a2c8
Sivasri234 <sivadhanyasri143@gmail.com>
2024-09-06 18:03:11 +0530

    modify hostel bed details

--- components/bed_details.js
+++ components/bed_details.js
@@ -4,35 +4,37 @@ function check_bed_details(bed_details_obj) {
 
     return new Promise((resolve, reject) => {
 
-        if (hostel_id == 0 || hostel_id == undefined && bed_details_obj.floor_id == 0 || bed_details_obj.floor_id == undefined && bed_details_obj.room == 0 || bed_details_obj.room == undefined) {
+        console.log(bed_details_obj, "====================");
+
+        if (bed_details_obj.old_hostel != bed_details_obj.hostel_id) {
+
+            var up_query1 = "SELECT *,bd.id AS bed_detail_id FROM hostelrooms AS hr JOIN bed_details AS bd ON bd.hos_detail_id=hr.id WHERE hr.Hostel_Id='" + bed_details_obj.hostel_id + "' AND bd.status=1 AND hr.isActive=1";
+            connection.query(up_query1, function (err, up_res1) {
+                if (err) {
+                    reject(err)
+                } else if (up_res1.length != 0) {
+                    var bed_details_id = up_res1[0].bed_detail_id;
+
+                    var sql1 = "UPDATE bed_details SET isfilled=0, user_id=0 WHERE id='" + bed_details_id + "'";
+                    console.log(sql1);
+                    connection.query(sql1, (err, up_data) => {
+                        if (err) {
+                            console.log(err);
+                            reject(err)
+                        } else {
+                            resolve("sucess")
+                        }
+                    })
+                } else {
+                    resolve("No need to Update Hostel Details")
+                }
+            })
+        } else if (bed_details_obj.floor_id == 0 || bed_details_obj.floor_id == undefined && bed_details_obj.room == 0 || bed_details_obj.room == undefined) {
             resolve("No Need to Validate in this User");
         } else {
 
-            console.log(bed_details_obj);
-
             if (bed_details_obj.old_floor == bed_details_obj.floor_id && bed_details_obj.old_room == bed_details_obj.room && bed_details_obj.old_bed == bed_details_obj.bed) {
                 resolve("Success")
-            } else if (bed_details_obj.old_hostel != bed_details_obj.hostel_id) {
-
-                var up_query1 = "SELECT *,bd.id AS bed_detail_id FROM hostelrooms AS hr JOIN bed_details AS bd ON bd.hos_detail_id=hr.id WHERE bd.bed_no='" + bed_details_obj.bed + "' AND hr.Hostel_Id='" + bed_details_obj.hostel_id + "' AND hr.Floor_Id='" + bed_details_obj.floor_id + "' AND hr.Room_Id='" + bed_details_obj.room + "' AND bd.status=1 AND hr.isActive=1";
-                connection.query(up_query1, function (err, up_res1) {
-                    if (err) {
-                        reject(err)
-                    } else if (up_res1.length != 0) {
-                        var bed_details_id = be_res[0].bed_detail_id;
-
-                        var sql1 = "UPDATE bed_details SET isfilled=0, user_id=0 WHERE id='" + bed_details_id + "'";
-                        connection.query(sql1, (err, up_data) => {
-                            if (err) {
-                                reject(err)
-                            } else {
-                                resolve("sucess")
-                            }
-                        })
-                    } else {
-                        reject("No need to Update Hostel Details")
-                    }
-                })
 
             } else {
                 var be_query1 = "SELECT *,bd.id AS bed_detail_id FROM hostelrooms AS hr JOIN bed_details AS bd ON bd.hos_detail_id=hr.id WHERE bd.bed_no='" + bed_details_obj.bed + "' AND hr.Hostel_Id='" + bed_details_obj.hostel_id + "' AND hr.Floor_Id='" + bed_details_obj.floor_id + "' AND hr.Room_Id='" + bed_details_obj.room + "' AND bd.isfilled=0 AND bd.status=1 AND hr.isActive=1";
