15e596553d113407e62d42a64a5abbb029fcf119
Josh <immanjosh4@gmail.com>
2024-10-07 11:28:57 +0530

    modify user join date

--- UserQueries.js
+++ UserQueries.js
@@ -19,7 +19,7 @@ function getUsers(connection, response, request) {
 
   // var sql1 = "SELECT COUNT(*) as totalItems FROM hostel WHERE created_by='" + userDetails.id + "' AND isActive=1;";
   var query =
-    "SELECT * FROM hosteldetails AS hstlDetails inner join hostel AS hstl on hstl.Hostel_Id=hstlDetails.id and hstl.isActive=true LEFT JOIN country_list AS cl ON hstl.country_code=cl.country_code WHERE hstlDetails.created_By ='" +
+    "SELECT *,DATE_FORMAT(hstl.joining_Date, '%Y-%m-%d') AS user_join_date FROM hosteldetails AS hstlDetails inner join hostel AS hstl on hstl.Hostel_Id=hstlDetails.id and hstl.isActive=true LEFT JOIN country_list AS cl ON hstl.country_code=cl.country_code WHERE hstlDetails.created_By ='" +
     userDetails.id +
     "' ORDER BY hstl.ID DESC";
   connection.query(query, function (error, hostelData) {
