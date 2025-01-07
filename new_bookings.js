a662dce4b97659aacfdba56ded7f44fcd2fd92f5
Josh <immanjosh4@gmail.com>
2025-01-07 11:08:31 +0530

    change bookings details

--- new_bookings.js
+++ new_bookings.js
@@ -18,7 +18,7 @@ function add_booking(req, res) {
 
     var { f_name, l_name, mob_no, email_id, address, joining_date, amount, hostel_id, id } = req.body;
 
-    if (!f_name || !mob_no || !joining_date || !amount || !hostel_id || !email_id) {
+    if (!f_name || !mob_no || !joining_date || !amount || !hostel_id) {
         return res.status(201).json({ statusCode: 201, message: "Missing Mandatory Fields" });
     }
 
@@ -32,40 +32,53 @@ function add_booking(req, res) {
                     return res.status(201).json({ statusCode: 201, message: "Unable to Get Phone Details", reason: err.message });
                 } else if (ph_data.length == 0) {
 
-                    var sql2 = "SELECT * FROM bookings WHERE email_id=? AND status=1 AND hostel_id=? AND id !=?";
-                    connection.query(sql2, [email_id, hostel_id, id], async function (err, em_res) {
-                        if (err) {
-                            return res.status(201).json({ statusCode: 201, message: "Unable to Get Email Details", reason: err.message });
-                        } else if (em_res.length == 0) {
+                    if (email_id) {
 
-                            let profile_url = 0;
+                        var sql2 = "SELECT * FROM bookings WHERE email_id=? AND status=1 AND hostel_id=? AND id !=?";
+                        connection.query(sql2, [email_id, hostel_id, id], async function (err, em_res) {
+                            if (err) {
+                                return res.status(201).json({ statusCode: 201, message: "Unable to Get Email Details", reason: err.message });
+                            } else if (em_res.length == 0) {
 
-                            if (!profile) {
-                                profile_url = req.body.profile || 0
+                                update_booking()
+
                             } else {
-                                try {
-                                    profile_url = await uploadImage.uploadProfilePictureToS3Bucket(
-                                        bucket_name, folderName, `${f_name}${timestamp}${profile[0].originalname}`, profile[0]
-                                    );
-                                    console.log(profile_url);  // Log the URL
-                                } catch (error) {
-                                    console.error("Error uploading profile picture: ", error);
-                                }
+                                return res.status(202).json({ statusCode: 202, message: "Email Id Already Exists" });
                             }
+                        })
 
-                            // var sql3 = "INSERT INTO bookings (first_name,last_name,joining_date,amount,hostel_id,phone_number,email_id,address,created_by,profile) VALUES (?)";
-                            var sql3 = "UPDATE bookings SET first_name=?,last_name=?,joining_date=?,amount=?,hostel_id=?,phone_number=?,email_id=?,address=?,profile=? WHERE id=?";
-                            connection.query(sql3, [f_name, l_name, joining_date, amount, hostel_id, mob_no, email_id, address, profile_url, id], function (err, ins_data) {
-                                if (err) {
-                                    return res.status(201).json({ statusCode: 201, message: "Unable to Add Booking Details", reason: err.message });
-                                } else {
-                                    return res.status(200).json({ statusCode: 200, message: "Booking Updated Successfully!" })
-                                }
-                            })
+                    } else {
+                        update_booking()
+                    }
+
+
+                    async function update_booking() {
+
+                        let profile_url = 0;
+
+                        if (!profile) {
+                            profile_url = req.body.profile || 0
                         } else {
-                            return res.status(202).json({ statusCode: 202, message: "Email Id Already Exists" });
+                            try {
+                                profile_url = await uploadImage.uploadProfilePictureToS3Bucket(
+                                    bucket_name, folderName, `${f_name}${timestamp}${profile[0].originalname}`, profile[0]
+                                );
+                                console.log(profile_url);  // Log the URL
+                            } catch (error) {
+                                console.error("Error uploading profile picture: ", error);
+                            }
                         }
-                    })
+
+                        // var sql3 = "INSERT INTO bookings (first_name,last_name,joining_date,amount,hostel_id,phone_number,email_id,address,created_by,profile) VALUES (?)";
+                        var sql3 = "UPDATE bookings SET first_name=?,last_name=?,joining_date=?,amount=?,hostel_id=?,phone_number=?,email_id=?,address=?,profile=? WHERE id=?";
+                        connection.query(sql3, [f_name, l_name, joining_date, amount, hostel_id, mob_no, email_id, address, profile_url, id], function (err, ins_data) {
+                            if (err) {
+                                return res.status(201).json({ statusCode: 201, message: "Unable to Add Booking Details", reason: err.message });
+                            } else {
+                                return res.status(200).json({ statusCode: 200, message: "Booking Updated Successfully!" })
+                            }
+                        })
+                    }
 
                 } else {
                     return res.status(203).json({ statusCode: 203, message: "Phone Number Already Exists" });
@@ -85,41 +98,52 @@ function add_booking(req, res) {
                     return res.status(201).json({ statusCode: 201, message: "Unable to Get Phone Details", reason: err.message });
                 } else if (ph_data.length == 0) {
 
-                    var sql2 = "SELECT * FROM bookings WHERE email_id=? AND status=1 AND hostel_id=?";
-                    connection.query(sql2, [email_id, hostel_id], async function (err, em_res) {
-                        if (err) {
-                            return res.status(201).json({ statusCode: 201, message: "Unable to Get Email Details", reason: err.message });
-                        } else if (em_res.length == 0) {
+                    if (email_id) {
 
-                            let profile_url = 0;
-
-                            if (!profile) {
-                                profile_url = req.body.profile || 0
+                        var sql2 = "SELECT * FROM bookings WHERE email_id=? AND status=1 AND hostel_id=?";
+                        connection.query(sql2, [email_id, hostel_id], async function (err, em_res) {
+                            if (err) {
+                                return res.status(201).json({ statusCode: 201, message: "Unable to Get Email Details", reason: err.message });
+                            } else if (em_res.length == 0) {
+                                insert_booking()
                             } else {
-                                try {
-                                    profile_url = await uploadImage.uploadProfilePictureToS3Bucket(
-                                        bucket_name, folderName, `${f_name}${timestamp}${profile[0].originalname}`, profile[0]
-                                    );
-                                    console.log(profile_url);  // Log the URL
-                                } catch (error) {
-                                    console.error("Error uploading profile picture: ", error);
-                                }
+                                return res.status(202).json({ statusCode: 202, message: "Email Id Already Exists" });
                             }
+                        })
+                    } else {
+                        insert_booking()
+                    }
 
-                            var sql3 = "INSERT INTO bookings (first_name,last_name,joining_date,amount,hostel_id,phone_number,email_id,address,created_by,profile) VALUES (?)";
-                            var params = [f_name, l_name, joining_date, amount, hostel_id, mob_no, email_id, address, created_by, profile_url]
 
-                            connection.query(sql3, [params], function (err, ins_data) {
-                                if (err) {
-                                    return res.status(201).json({ statusCode: 201, message: "Unable to Add Booking Details", reason: err.message });
-                                } else {
-                                    return res.status(200).json({ statusCode: 200, message: "Booking Added Successfully!" })
-                                }
-                            })
+                    async function insert_booking() {
+
+                        let profile_url = 0;
+
+                        if (!profile) {
+                            profile_url = req.body.profile || 0
                         } else {
-                            return res.status(202).json({ statusCode: 202, message: "Email Id Already Exists" });
+                            try {
+                                profile_url = await uploadImage.uploadProfilePictureToS3Bucket(
+                                    bucket_name, folderName, `${f_name}${timestamp}${profile[0].originalname}`, profile[0]
+                                );
+                                console.log(profile_url);  // Log the URL
+                            } catch (error) {
+                                console.error("Error uploading profile picture: ", error);
+                            }
                         }
-                    })
+
+                        var sql3 = "INSERT INTO bookings (first_name,last_name,joining_date,amount,hostel_id,phone_number,email_id,address,created_by,profile) VALUES (?)";
+                        var params = [f_name, l_name, joining_date, amount, hostel_id, mob_no, email_id, address, created_by, profile_url]
+
+                        connection.query(sql3, [params], function (err, ins_data) {
+                            if (err) {
+                                return res.status(201).json({ statusCode: 201, message: "Unable to Add Booking Details", reason: err.message });
+                            } else {
+                                return res.status(200).json({ statusCode: 200, message: "Booking Added Successfully!" })
+                            }
+                        })
+                    }
+
 
                 } else {
                     return res.status(203).json({ statusCode: 203, message: "Phone Number Already Exists" });
