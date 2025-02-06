6579ff9ee6e0db579dd0a5295fea142a6bf6d8bb
Josh <immanjosh4@gmail.com>
2025-02-06 12:52:55 +0530

    change user id

--- index.js
+++ index.js
@@ -43,7 +43,9 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 app.options('*', cors());
+
 app.use(express.json())
+
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Accept");
