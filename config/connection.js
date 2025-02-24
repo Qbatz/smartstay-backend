2f1722bc688150a6e6769a097855d2a4ceabe853
Josh <immanjosh4@gmail.com>
2025-02-24 16:28:38 +0530

    db changes

--- config/connection.js
+++ config/connection.js
@@ -1,34 +1,14 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
-let conn;
+const pool = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.HOST_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,  // Allow 10 concurrent connections
+    queueLimit: 0,
+    multipleStatements: true
+});
 
-function handleDisconnect() {
-    conn = mysql.createConnection({
-        host: process.env.HOST,
-        user: process.env.HOST_USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
-        multipleStatements: true
-    }); 
-    console.log('Database Details:', process.env.HOST, process.env.HOST_USER, process.env.PASSWORD, process.env.DATABASE);
-
-    conn.connect((err) => {
-        if (err) {
-            console.error('Error connecting to database:', err.message);
-            setTimeout(handleDisconnect, 2000);
-        }
-    });
-
-    conn.on('error', (err) => {
-        console.error('Database error:', err.message);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleDisconnect();
-        } else {
-            throw err;
-        }
-    });
-}
-
-handleDisconnect();
-
-module.exports = conn;
+module.exports = pool; // Use async/await for queries
