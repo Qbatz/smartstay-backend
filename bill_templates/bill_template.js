1cbedd4d2b9a93dee166762346b0cb0f5fc862dd
s3remotica <s3Remotica@gmail.com>
2025-07-30 18:53:32 +0530

    BillTemplateSetting function created

--- bill_templates/bill_template.js
+++ bill_templates/bill_template.js
@@ -203,7 +203,37 @@ FROM bill_template where hostel_Id=?;`
 async function FetchTemplateListDetails(req, res) {
 
   const { hostel_Id } = req.body;
-  const Query = `SELECT * FROM bill_template where hostel_Id=?;`
+  const Query = `SELECT 
+  bt.*,
+  IF(
+    b.id IS NOT NULL,
+    JSON_OBJECT(
+      'id', b.id,
+      'acc_num', b.acc_num,
+      'ifsc_code', b.ifsc_code,
+      'bank_name', b.bank_name,
+      'acc_name', b.acc_name,
+      'description', b.description,
+      'setus_default', b.setus_default,
+      'balance', b.balance,
+      'hostel_id', b.hostel_id,
+      'status', b.status,
+      'type', b.type,
+      'benificiary_name', b.benificiary_name,
+      'upi_id', b.upi_id,
+      'card_type', b.card_type,
+      'card_holder', b.card_holder,
+      'card_no', b.card_no
+    ),
+    NULL
+  ) AS banking
+FROM 
+  bill_template bt
+LEFT JOIN 
+  bankings b ON bt.banking_id = b.id
+WHERE 
+  bt.hostel_Id = ?;
+`
 
   connection.query(Query, [hostel_Id], (err, result) => {
     if (err) {
@@ -360,175 +390,6 @@ async function BillTemplateSetting(req, res) {
 
   }
   catch (error) {
-    console.error("Unexpected Error:", error);
-    return res.status(500).json({
-      statusCode: 500,
-      message: "Something went wrong while processing invoice settings.",
-    });
-  }
-}
-
-async function BillTemplateSettings(req, res) {
-  try {
-    const timestamp = Date.now();
-    const bucketName = process.env.AWS_BUCKET_NAME;
-    const folderName = "Hostel-Payments/";
-    const template_type = [
-      "Rental Invoice",
-      "Security Deposit Invoice",
-      "Rental Receipt",
-      "Security Deposit Receipt",
-      "NOC Receipt",
-    ];
-
-    const {
-      is_logo_specific_template,
-      contact_number,
-      is_contact_specific_template,
-      email,
-      is_email_specific_template,
-      is_signature_specific_template,
-      hostel_Id,
-      id,
-    } = req.body;
-
-    const logoFile = req.files?.["logo_url"]?.[0] || null;
-    const signatureFile = req.files?.["digital_signature_url"]?.[0] || null;
-
-    let signatureUrl = null;
-    let logoFileUrl = null;
-
-    if (signatureFile) {
-      const fileName = `${hostel_Id}_${timestamp}_${signatureFile.originalname}`;
-      signatureUrl = await uploadImage.uploadProfilePictureToS3Bucket(
-        bucketName,
-        folderName,
-        fileName,
-        signatureFile
-      );
-    }
-
-    if (logoFile) {
-      const fileName = `${hostel_Id}_${timestamp}_${logoFile.originalname}`;
-      logoFileUrl = await uploadImage.uploadProfilePictureToS3Bucket(
-        bucketName,
-        folderName,
-        fileName,
-        logoFile
-      );
-    }
-
-    if (!hostel_Id) {
-      return res
-        .status(201)
-        .json({ statusCode: 201, message: "hostelId is required" });
-    }
-    const toBool = (val) => val === "true" || val === true;
-    const sqlSelect = `select * from bill_template where hostel_Id=?;`;
-    connection.query(sqlSelect, [hostel_Id], (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        if (result.length > 0) {
-          const updatesql = `
-                UPDATE bill_template SET
-                common_logo_url =?,
-                    logo_url = ?,
-                    is_logo_specific_template = ?,
-                    common_contact_number = ?,
-                    contact_number = ?,
-                    is_contact_specific_template = ?,
-                    common_email = ?,
-                    email = ?,
-                    is_email_specific_template = ?,
-                    common_digital_signature_url = ?,
-                    digital_signature_url = ?,
-                    is_signature_specific_template = ?
-                WHERE hostel_Id = ? AND template_type = ? AND id =? 
-`;
-
-          let completed = 0;
-          template_type.forEach((type) => {
-            const values = [
-              logoFileUrl,
-              logoFileUrl,
-              toBool(is_logo_specific_template),
-              contact_number,
-              contact_number,
-              toBool(is_contact_specific_template),
-              email,
-              email,
-              toBool(is_email_specific_template),
-              signatureUrl,
-              signatureUrl,
-              toBool(is_signature_specific_template),
-              hostel_Id,
-              type,
-            ];
-
-            connection.query(updatesql, values, (err, result) => {
-              if (err) {
-                console.log(err);
-                return res.status(500).json({ error: err.message });
-              }
-              completed++;
-              if (completed === template_type.length) {
-                return res.status(200).json({
-                  successCode: 200,
-                  message:
-                    "Invoice settings Bill Template updated successfully.",
-                });
-              }
-            });
-          });
-        } else {
-          const insertsql = `
-                        INSERT INTO bill_template (
-                        common_logo_url,
-                            logo_url, is_logo_specific_template,
-                            common_contact_number,contact_number, is_contact_specific_template,
-                            common_email,email, is_email_specific_template,
-                            common_digital_signature_url, digital_signature_url,is_signature_specific_template,
-                            hostel_Id, template_type
-                        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?,?,?)
-`;
-          let completedInsert = 0;
-          template_type.forEach((type) => {
-            const values = [
-              logoFileUrl,
-              logoFileUrl,
-              toBool(is_logo_specific_template),
-              contact_number,
-              contact_number,
-              toBool(is_contact_specific_template),
-              email,
-              email,
-              toBool(is_email_specific_template),
-              signatureUrl,
-              signatureUrl,
-              toBool(is_signature_specific_template),
-              hostel_Id,
-              type,
-            ];
-
-            connection.query(insertsql, values, (err, result) => {
-              if (err) {
-                console.error("Insert failed for:", type, err.message);
-                return;
-              }
-              completedInsert++;
-              if (completedInsert === template_type.length) {
-                return res.status(200).json({
-                  successCode: 200,
-                  message: "Invoice settings Bill Template added successfully.",
-                });
-              }
-            });
-          });
-        }
-      }
-    });
-  } catch (error) {
     console.error("Unexpected Error:", error);
     return res.status(500).json({
       statusCode: 500,
