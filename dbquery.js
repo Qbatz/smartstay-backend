8bb6ae3005759c324c5733640b52c959229d6288
Qbatz <qbatz.tech@gmail.com>
2025-09-03 15:43:54 +0530

    Checout added extra field

--- dbquery.js
+++ dbquery.js
@@ -1,9 +1,10 @@
 const connection = require("./config/connection");
 
 // Usage
-const queries = [];
+const queries = ['ALTER TABLE receipts ADD COLUMN transaction_id VARCHAR(100)'];
 
 queries.forEach(executeQuery);
+
 
 function executeQuery(sql) {
   connection.query(sql, function (err, data) {
