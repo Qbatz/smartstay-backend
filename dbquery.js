a1a695e5d4013edec7c2215418e3c9679e9df760
Qbatz <qbatz.tech@gmail.com>
2025-10-04 18:13:50 +0530

    Bill write Off changes

--- dbquery.js
+++ dbquery.js
@@ -2,7 +2,8 @@ const connection = require("./config/connection");
 
 // Usage
 const queries = ['ALTER TABLE receipts ADD COLUMN transaction_id VARCHAR(100)',
-  'ALTER TABLE hostel ADD COLUMN kyc_docs JSON NULL;'
+  'ALTER TABLE hostel ADD COLUMN kyc_docs JSON NULL;',
+  'ALTER TABLE invoicedetails ADD COLUMN reason VARCHAR(255);'
 ];
 
 queries.forEach(executeQuery);
