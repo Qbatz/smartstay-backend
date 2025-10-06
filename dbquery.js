34378c704f6bcb30f0783f408ea659e902c12f54
Qbatz <qbatz.tech@gmail.com>
2025-10-06 14:33:30 +0530

    Manual Doc

--- dbquery.js
+++ dbquery.js
@@ -3,7 +3,8 @@ const connection = require("./config/connection");
 // Usage
 const queries = ['ALTER TABLE receipts ADD COLUMN transaction_id VARCHAR(100)',
   'ALTER TABLE hostel ADD COLUMN kyc_docs JSON NULL;',
-  'ALTER TABLE invoicedetails ADD COLUMN reason VARCHAR(255);'
+  'ALTER TABLE invoicedetails ADD COLUMN reason VARCHAR(255);',
+  'ALTER TABLE hostel ADD COLUMN manual_docs JSON NULL;'
 ];
 
 queries.forEach(executeQuery);
